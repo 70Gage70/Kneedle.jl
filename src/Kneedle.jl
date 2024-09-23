8ab213ae9f0d39408b315a2f628aa126d88375e1
Gage <gage.bonner@gmail.com>
2024-09-23 18:01:35 -0500

    docs

--- src/Kneedle.jl
+++ src/Kneedle.jl
@@ -1,6 +1,7 @@
 module Kneedle
 
 using ArgCheck, Loess
+import Random
 using PrecompileTools: @compile_workload
 
 include("testers.jl")
@@ -14,16 +15,12 @@ include("viz.jl")
 export viz
     
 @compile_workload begin
-    x, y = Testers.double_bump()
+    Random.seed!(1234)
+    x, y = Testers.double_bump(noise_level = 0.1)
     kneedle(x, y, smoothing = 0.1)
-    x, y = Testers.CONVEX_INC
-    kneedle(x, y, "convex_inc")
-    x, y = Testers.CONVEX_DEC
-    kneedle(x, y, "convex_dec")
-    x, y = Testers.CONCAVE_INC
-    kneedle(x, y, "concave_inc")
-    x, y = Testers.CONCAVE_DEC
-    kneedle(x, y, "concave_dec") 
+    kneedle(x, y, "|¯", S = 0.1)
+    kneedle(x, y, "|¯", 2, scan_type = :S)
+    kneedle(x, y, "|¯", 2, scan_type = :smoothing)
 end
 
 end # module
--- src/main.jl
+++ src/main.jl
@@ -1,9 +1,13 @@
 """
-    struct KneedleResult{X, Y}
+    struct KneedleResult{X}
 
 A container for the output of the Kneedle algorithm.
 
-This object is returned by `kneedle`.
+Use [`knees`](@ref) to access the `knees` field.
+
+Refer to [`viz`](@ref) for visualization.
+
+See the [`kneedle`](@ref) function for further general information.
 
 ### Fields 
 
@@ -21,21 +25,101 @@ end
     knees(kr::KneedleResult)
 
 Return `kr.knees`.
+
+Refer to [`KneedleResult`](@ref) or [`kneedle`](@ref) for more information.
 """
 knees(kr::KneedleResult) = kr.knees
 
 """
-    kneedle
+    kneedle(args...,)
+    
+There are several methods for the `kneedle` function as detailed below; each returns a [`KneedleResult`](@ref). 
+
+Use `knees(kr::KneedleResult)` to obtain the computed knees/elbows as a list of `x` coordinates.
+
+Refer to [`viz`](@ref) for visualization.
+
+Each `kneedle` function contains the args `x` and `y` which refer to the input data. It is required that `x` is sorted.
+
+Each `kneedle` function contains the kwargs `S` and `smoothing`. `S > 0` refers to the sensitivity of the knee/elbow \
+detection algorithm in the sense that higher `S` results in fewer detections. `smoothing` refers to the amount of \
+smoothing via interpolation that is applied to the data before knee detection. If `smoothing == nothing`, it will \
+be bypassed entirely. If `smoothing ∈ [0, 1]`, this parameter is passed directly to \
+[Loess.jl](https://github.com/JuliaStats/Loess.jl) via its `span` parameter. Generally, higher `smoothing` results \
+in less detection.
+
+## Shapes
+
+There are four possible knee/elbow shapes in consideration. If a `kneedle` function takes `shape` as an argument, it \
+should be one of these.
+
+- concave increasing: `|¯` or `"concave_inc"`
+- convex decreasing: `|_` or `"convex_dec"`
+- concave decreasing: `¯|` or `"concave_dec"`
+- convex increasing: `_|` or `"convex_inc"`
+
+Note that the symbol `¯` is entered by typing `\\highminus<TAB>`
+
+## Methods
+
+### Fully automated kneedle
+
+    kneedle(x, y; S = 1.0, smoothing = nothing, verbose = false)
+
+This function attempts to determine the shape of the knee automatically. Toggle `verbose` to get a printout of \
+the guessed shape.
+
+### Kneedle with a specific shape
+
+    kneedle(x, y, shape; S = 1.0, smoothing = nothing)
+
+This function finds knees/elbows with the given `shape`.
+
+### Kneedle with a specific shape and number of knees
+
+    kneedle(x, y, shape, n_knees; scan_type = :S, S = 1.0, smoothing = nothing)
+
+This function finds exactly `n_knees` knees/elbows with the given `shape`.
+
+This works by bisecting either `S` (if `scan_type == :S`) or `smoothing` (if `scan_type == :smoothing`).
+
+## Examples
+
+Find a knee:
+
+```julia-repl
+julia> x, y = Testers.CONCAVE_INC
+julia> kr1 = kneedle(x, y)
+julia> knees(kr1) # 2, meaning that there is a knee at `x = 2`
+```
+
+Find a knee with a specific shape:
+
+```julia-repl
+julia> kr2 = kneedle(x, y, "concave_inc")
+julia> knees(kr1) == knees(kr2) # true
+```
+
+Use the pictoral arguments:
+
+```julia-repl
+julia> kr3 = kneedle(x, y, "|¯")
+julia> knees(kr3) == knees(kr1) # true
+```
+
+Find a given number of knees:
+
+```julia-repl
+julia> x, y = Testers.double_bump(noise_level = 0.3)
+julia> kr4 = kneedle(x, y, "|¯", 2)
+julia> length(knees(kr4)) # 2, meaning that the algorithm found 2 knees
+```
 """
 function kneedle end
 
-"""
-	_kneedle(x, y; S = 1.0, smoothing = nothing)
 
-Compute the main Kneedle algorithm and return a [`KneedleResult`](@ref).
-
-Assumes that data is concave increasing, i.e. `|¯` and that `x` is sorted.
-"""
+# C ompute the main Kneedle algorithm and return a [`KneedleResult`](@ref).
+# Assumes that data is concave increasing, i.e. `|¯` and that `x` is sorted.
 function _kneedle(
 	x::AbstractVector{<:Real}, 
 	y::AbstractVector{<:Real}; 
@@ -90,6 +174,7 @@ function _kneedle(
     return KneedleResult(collect(float(x_s)), collect(float(y_s)), knees_res)
 end
 
+# find knees with a particular shape
 function kneedle(
     x::AbstractVector{<:Real}, 
     y::AbstractVector{<:Real},
@@ -124,6 +209,7 @@ function kneedle(
     
 end
 
+# try to guess the shape automatically
 function kneedle(
     x::AbstractVector{<:Real}, 
     y::AbstractVector{<:Real}; 
@@ -151,6 +237,7 @@ function kneedle(
     end
 end
 
+# use bisection to find a given number of knees by varying the sensitivity
 function _kneedle_scan_S(
     x::AbstractVector{<:Real}, 
     y::AbstractVector{<:Real},
@@ -161,12 +248,22 @@ function _kneedle_scan_S(
     # usually, higher S means less knees; define 1/s here since easier to keep track of
     _n_knees(s) = kneedle(x, y, shape, S = 1/s, smoothing = smoothing) |> x -> length(knees(x))
     lb, ub = 0.1, 10.0
+    n_iters = 0
     while _n_knees(lb) > n_knees
         lb = lb/2
+        n_iters += 1
+        if n_iters == 10
+            error("Could not find the requested number of knees (requested too few).")
+        end
     end
 
+    n_iters = 0
     while _n_knees(ub) < n_knees
         ub = ub*2
+        n_iters += 1
+        if n_iters == 10
+            error("Could not find the requested number of knees (requested too many.)")
+        end
     end
 
     a, b = lb, ub
@@ -192,6 +289,7 @@ function _kneedle_scan_S(
     return kneedle(x, y, shape, S = 1/c, smoothing = smoothing)
 end
 
+# use bisection to find a given number of knees by varying the smoothing
 function _kneedle_scan_smooth(
     x::AbstractVector{<:Real}, 
     y::AbstractVector{<:Real},
@@ -202,9 +300,13 @@ function _kneedle_scan_smooth(
     # usually, higher smoothing means less knees; define 1/s here since easier to keep track of
     _n_knees(s) = kneedle(x, y, shape, S = S, smoothing = 1/s) |> x -> length(knees(x))
     ub = 10.0
-
+    n_iters = 0
     while _n_knees(ub) < n_knees
         ub = ub*2
+        n_iters += 1
+        if n_iters == 10
+            error("Could not find the requested number of knees (requested too many).")
+        end
     end
 
     a, b = 1.0, ub
@@ -230,6 +332,7 @@ function _kneedle_scan_smooth(
     return kneedle(x, y, shape, S = S, smoothing = 1/c)
 end
 
+# find a given number of knees by searching either the sensitivity or the smoothing
 function kneedle(
     x::AbstractVector{<:Real}, 
     y::AbstractVector{<:Real},
